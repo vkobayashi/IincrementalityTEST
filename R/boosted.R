0e2e63aaa6a9a9df43aeeaabce56e88bc019a438
vkobayashi <vladimer.kobayashi@gmail.com>
2016-09-02 00:44:48 +0100

    deleted incrementality_func_hypothesis function

--- R/boosted.R
+++ R/boosted.R
@@ -11,20 +11,6 @@ incrementality_func<-function(datadiff, indices){
   c(mean(d), var(d)/length(d))
 }
 
-#' Hypothesis Testing Bootstrap
-#'
-#' This function construct the sampling distribution of a metric under a null effect.
-#' @param datadiff Differences of the chosen metric centered from the mean of the difference
-#' @param indices The indices of the sampled rows
-#' @return Bootstrap mean and standard error
-#' @export
-
-incrementality_func_hypothesis<-function(datadiff, indices){
-  dwinsor<-winsor(datadiff, trim=0.01)
-  d<-dwinsor[indices]
-  c(mean(d), var(d)/length(d))
-}
-
 #' Boostrap routine
 #'
 #' This function runs the bootstrap routine.
